be49b0a7d2a5d55cee26b5bc47f58501ac305883
Sledziux <sledziuxjp@gmail.com>
2021-01-16 13:08:02 +0100

    Updates

---
+++ precache-manifest.26d6cc08b40a006e7e29dd1bcd404438.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3790250f8c0e19a77fba",
+    "url": "/app-rr/static/css/main.bddd7128.chunk.css"
+  },
+  {
+    "revision": "3790250f8c0e19a77fba",
+    "url": "/app-rr/static/js/main.26cc7ece.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "c7ff2f40b1982aa653a6",
+    "url": "/app-rr/static/js/2.9f381332.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "e4137ebf81e1a9fecef10cf9fd62e93e",
+    "url": "/app-rr/index.html"
+  }
+];
